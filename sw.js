59ae1d9dd759fcf3cfb6582e9fed61a77ccf204d
oomh2025-artifact <oomh2025@gmail.com>
2026-09-23 10:34:55 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+// 労働衛生ドリル: 通信なしで動かすための保存（キャッシュ）
+const VERSION = '9ed3302ebf';
+const CACHE = 'lwd-' + VERSION;
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-192.png', './icon-maskable-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;   // フォントなど外部は通常どおり
+  e.respondWith(caches.match(e.request, { ignoreSearch: true }).then(r => r || fetch(e.request).then(res => {
+    if (e.request.method === 'GET' && res.ok) { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); }
+    return res;
+  })));
+});
+self.addEventListener('message', e => { if (e.data === 'skipWaiting') self.skipWaiting(); });
